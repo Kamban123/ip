70efa0692a3b73401a719d15d9fb36fb38d42b1f
Kamban123 <kamban.sg@gmail.com>
2026-08-22 14:16:57 +0800

    Added greeting and bye

--- src/main/java/Nabmak.java
+++ src/main/java/Nabmak.java
@@ -1,10 +1,15 @@
 public class Nabmak {
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String banner = "________________________________________";
+        String name = "Nabmak";
+        String intro = "Yo im " + name + ".";
+        String greeting = "Whatchu want?";
+        String bye = "BYE!";
+        
         System.out.println(banner);
+        System.out.println(intro);
+        System.out.println(greeting);
+        System.out.println(banner);
+        System.out.println(bye);
     }
 }
