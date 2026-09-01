62635409482305bb21ffa205a05914859ad330f7
Kamban123 <kamban.sg@gmail.com>
2026-09-01 10:47:22 +0800

    Add full javadocs

--- src/main/java/nabmak/task/ToDo.java
+++ src/main/java/nabmak/task/ToDo.java
@@ -1,11 +1,23 @@
 package nabmak.task;
 
+/**
+ * Represents a todo task with a description.
+ */
 public class ToDo extends Task {
-
+    /**
+     * Creates a todo task with a description.
+     *
+     * @param desc description of task
+     */
     public ToDo(String desc) {
         super(desc);
     }
 
+    /**
+     * Converts task to user readable string format.
+     *
+     * @return todo task in string format
+     */
     @Override
     public String toString() {
         return "[To Do]" + super.toString();
--- src/main/java/nabmak/ui/Ui.java
+++ src/main/java/nabmak/ui/Ui.java
@@ -3,29 +3,47 @@ package nabmak.ui;
 import nabmak.task.Task;
 import nabmak.task.TaskList;
 
+/**
+ * Provides the dialgoues for the application to interact with the user.
+ */
 public class Ui {
     private static final String BANNER = "________________________________________";
     private static final String INTRO = "Yo im Nabmak.";
     private static final String GREETING = "Whatchu wanna do?";
 
+    /**
+     * Prints the greeting dialogue.
+     */
     public void showHi() {
         System.out.println(BANNER);
         System.out.println(INTRO);
         System.out.println(GREETING);
         System.out.println(BANNER);
     }
-
+    /**
+     * Prints the goodbye dialogue.
+     */
     public void showBye() {
         System.out.println(BANNER);
         System.out.println("BYE!");
     }
 
+    /**
+     * Prints error message for invalid user input.
+     *
+     * @param message error message 
+     */
     public void showError(String message) {
         System.out.println(BANNER);
         System.out.println("TOUGH! " + message);
         System.out.println(BANNER);
     }
 
+    /**
+     * Prints list of existing tasks.
+     *
+     * @param tasks existing list of tasks
+     */
     public void showList(TaskList tasks) {
         System.out.println(BANNER);
         System.out.println("Your TODOLIST");
@@ -36,18 +54,34 @@ public class Ui {
         System.out.println(BANNER);
     }
 
+    /**
+     * Prints result when task is marked.
+     *
+     * @param task task being marked
+     */
     public void showDone(Task task) {
         System.out.println("Good that task is DONE");
         System.out.println(task);
         System.out.println(BANNER);
     }
 
+    /**
+     * Prints result when task is unmarked.
+     *
+     * @param task task being unmarked
+     */
     public void showUndone(Task task) {
         System.out.println("Tuff this task not done :(");
         System.out.println(task);
         System.out.println(BANNER);
     }
 
+    /**
+     * Prints dialogue for adding a new task and number of tasks in list.
+     *
+     * @param task task being added
+     * @param size number of tasks in list
+     */
     public void showAdded(Task task, int size) {
         System.out.println(BANNER);
         System.out.println("Ok new task!");
@@ -55,7 +89,12 @@ public class Ui {
         System.out.println("Now got " + size + " tasks.");
         System.out.println(BANNER);
     }
-
+    /**
+     * Prints dialogue for deleting a task and number of tasks remaining in list.
+     *
+     * @param task task being deleted
+     * @param size number of tasks remaining in list
+     */
     public void showDeleted(Task deleted, int size) {
         System.out.println(BANNER);
         System.out.println("Noted. I've remove the task:");
